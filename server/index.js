54b7bf99292826ce448bf843c92e40c7b5e229ac
m0m0i <hirocorp01@gmail.com>
2023-05-03 18:23:25 +0900

    add verify signature feature

--- server/index.js
+++ server/index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { verifySignature } = require("./ctypto");
 const port = 3042;
 
 app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  c72c9e4bbd61ecf3e57534783daf958103ca93f4: 100,
+  "04dff1d01ce64eb6aab046eb370e3d44ae54acac": 50,
+  b634120feb8c47fae54bd3016d2990a1279f5cf9: 75,
 };
 
 app.get("/balance/:address", (req, res) => {
@@ -19,12 +20,18 @@ app.get("/balance/:address", (req, res) => {
 });
 
 app.post("/send", (req, res) => {
-  const { sender, recipient, amount } = req.body;
+  const { message, sigHex } = req.body;
+
+  const { sender, recipient, publicKey, amount } = message;
+
+  const isSigned = verifySignature(sigHex, message, publicKey);
 
   setInitialBalance(sender);
   setInitialBalance(recipient);
 
-  if (balances[sender] < amount) {
+  if (!isSigned) {
+    res.status(400).send({ message: "Wrong Signature Detected!" });
+  } else if (balances[sender] < amount) {
     res.status(400).send({ message: "Not enough funds!" });
   } else {
     balances[sender] -= amount;
