a9f6840f0007f56bb80c4fa62eeccc4eb90fa69e
m0m0i <hirocorp01@gmail.com>
2023-05-03 18:20:26 +0900

    create state for the pk and sk

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,8 @@ import { useState } from "react";
 function App() {
   const [balance, setBalance] = useState(0);
   const [address, setAddress] = useState("");
+  const [publicKey, setPublicKey] = useState("");
+  const [privateKey, setPrivateKey] = useState("");
 
   return (
     <div className="app">
@@ -14,8 +16,15 @@ function App() {
         setBalance={setBalance}
         address={address}
         setAddress={setAddress}
+        setPublicKey={setPublicKey}
+        setPrivateKey={setPrivateKey}
       />
-      <Transfer setBalance={setBalance} address={address} />
+      <Transfer
+        setBalance={setBalance}
+        address={address}
+        publicKey={publicKey}
+        privateKey={privateKey}
+      />
     </div>
   );
 }
